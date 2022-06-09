e15da85a34f2cea2545f037721c1b12e9873b279
DmitriyBarinov <95102030+Defolt023@users.noreply.github.com>
2022-06-09 17:07:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,68 +3,68 @@
 #include <map>
 #include "tstack.h"
 std::string infx2pstfx(std::string inf) {
-  std::string strrr = "";
+std::string s = "";
   TStack<char, 100> Stack;
   std::map<char, int> Prior;
-  Prior['('] = 0;
   Prior[')'] = 0;
-  Prior['+'] = 1;
+  Prior['('] = 0;
   Prior['-'] = 1;
   Prior['*'] = 2;
+  Prior['+'] = 1;
   Prior['/'] = 2;
-  for (int i = 0; i < inf.size(); i++) {
-  if (Prior.find(inf[i]) == Prior.end()) {
-  strrr += inf[i];
-  strrr += ' ';
+  for (int j = 0; j < inf.size(); j++) {
+  if (Prior.find(inf[j]) == Prior.end()) {
+  s += inf[j];
+  s += ' ';
   } else {
-  if (inf[i] == ')') {
+  if (inf[j] == ')') {
   while (Stack.get() != '(') {
-  strrrv += Stack.get();
-  strrr += ' ';
+  s += Stack.get();
+  s += ' ';
   Stack.pop();
   }
   Stack.pop();
   } else {
-  if (inf[i] == '(' || Stack.Empty()) {
-  Stack.push(inf[i]);
+  if (inf[j] == '(' || Stack.Empty()) {
+  Stack.push(inf[j]);
   } else {
-  if (Prior[inf[i]] > Prior[Stack.get()]) {
-  Stack.push(inf[i]);
+  if (Prior[inf[j]] > Prior[Stack.get()]) {
+  Stack.push(inf[j]);
   } else {
-  while (Prior[inf[i]] <= Prior[Stack.get()]) {
+  while (Prior[inf[j]] <= Prior[Stack.get()]) {
   if (!Stack.Empty()) {
-  strrr += Stack.get();
-  strrr += ' ';
+  s += Stack.get();
+  s += ' ';
   Stack.pop();
   }
   }
-  Stack.push(inf[i]);
+  Stack.push(inf[j]);
   }
   }
   }
   }
   }
   while (!Stack.Empty()) {
-  strrr += Stack.get();
-  strrr += ' ';
+  s += Stack.get();
+  s += ' ';
   Stack.pop();
   }
-  strrr.pop_back();
-  return strrr;
+  s.pop_back();
+  return s;
 }
 int eval(std::string pref) {
   TStack<int, 100> Stack;
-  int val1, val2, result, i = 0;
-  while (i < pref.size()) {
-  if (pref[i] >= '0') {
-  Stack.push(pref[i] - '0');
+  int val1, val2, result, j = 0;
+  while (j < pref.size()) {
+  if (pref[j] >= '0') {
+  Stack.push(pref[j] - '0');
   } else {
-  if (pref[i] != ' ') {
+  if (pref[j] != ' ') {
   val2 = Stack.get();
   Stack.pop();
   val1 = Stack.get();
   Stack.pop();
-  switch (pref[i]) {
+  switch (pref[j]) {
   case '+':
   result = val1 + val2;
   break;
@@ -84,4 +84,3 @@ int eval(std::string pref) {
   i++;
   }
   return Stack.get();
-}
